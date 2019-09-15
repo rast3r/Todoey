fbac3a87ee65e5939099273770c85aec930854a5
rast3r <ar.banasiewicz@gmail.com>
2019-09-15 16:44:42 +0200

    todo list with datasource and delegates

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -31,5 +31,22 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
+    // MARK - TableView delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+           tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
 }
 
