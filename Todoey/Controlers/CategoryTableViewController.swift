83cf92269a8bf8e50d47110da8be890ef67b62bb
rast3r <ar.banasiewicz@gmail.com>
2019-09-22 15:56:22 +0200

    RealmSwift pod installed

---
+++ Todoey/Controlers/CategoryTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by menestys on 21/09/2019.
+//  Copyright © 2019 Adam Banasiewicz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+
+    var categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadCategories()
+    }
+
+    //MARK: - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categoryArray[indexPath.row].name
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+       if let indexPath = tableView.indexPathForSelectedRow {
+        destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
+        
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        }
+        catch {
+            print("Error saving category \(error)")
+        }
+
+        tableView.reloadData()
+    }
+    
+    func loadCategories() {
+        
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+        categoryArray = try context.fetch(request)
+        } catch {
+            print("Error loading category \(error)")
+        }
+        
+       tableView.reloadData()
+    }
+    
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+       
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newCategory = Category(context: self.context)
+            
+            newCategory.name = textField.text!
+            
+            self.categoryArray.append(newCategory)
+
+            self.saveCategories()
+        
+        }
+        
+        alert.addAction(action)
+      
+        alert.addTextField { (field) in
+            textField = field
+            field.placeholder = "Add new Category"
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
